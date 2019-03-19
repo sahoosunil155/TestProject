3bb0f48a219ef93dde56244e18fbb1a444b8ec1d
sahoosunil155 <sahoosunil155@gmail.com>
2019-03-19 15:07:12 +0800

    Added a class Test

---
+++ TestProject/src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+	String s1="foobar";
+	String s2="foo";
+	s2=s2+"bar";
+	System.out.println(s2);
+	System.out.println(s1==s2);
+	System.out.println(s1.equals(s2));
+	
+
+	}
+
+}
